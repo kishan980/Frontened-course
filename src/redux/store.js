039066f734d66da5cf1547a3c7082d19e912e84a
kishan980 <Kishany980@gmail.com>
2023-02-13 11:08:15 +0530

    add

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,4 +8,4 @@ const store = configureStore({
 })
 
 export default store;
-export const server = "https://node-test-liu0.onrender.com";
+export const server = "https://node-test-bafi.onrender.com";
